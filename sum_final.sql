80051b7d82522fa100ec9741e31cf37f137871f2
solaimooon <118716931+solaimooon@users.noreply.github.com>
2025-01-25 19:01:15 +0330

    set two decomal digit

--- sum_final.sql
+++ sum_final.sql
@@ -1,7 +1,9 @@
-create or replace view SUM_final as 
- SELECT ROW_NUMBER() OVER (ORDER BY enroll_id ) AS id,
- enroll_id,
- sum(sumed_emtiyaz)AS SUM
- FROM sum_emtiyazat_for_each_day n
- group by enroll_id
- 
+CREATE OR REPLACE VIEW SUM_final AS
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY enroll_id) AS id,
+    enroll_id,
+    ROUND(SUM(sumed_emtiyaz), 2) AS SUM -- محدود کردن جمع به دو رقم اعشار
+FROM 
+    sum_emtiyazat_for_each_day n
+GROUP BY 
+    enroll_id;
