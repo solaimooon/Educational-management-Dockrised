8aa2845dde3b5376c650fc4e20297b1b106a3773
solaimooon <118716931+solaimooon@users.noreply.github.com>
2025-02-24 16:37:46 +0000

    add remazan module for emtiyazat mahmobarak

---
+++ ramazan/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django_jalali.db import models as jmodels
+from django.contrib.auth.models import User
+class type_emtiyaz(models.Model):
+    name=models.CharField(max_length=50)
+    max_point=models.IntegerField()
+class Time_period(models.Model):
+    name=models.CharField(max_length=50)    
+class ramazan_point(models.Model):
+    amount=models.IntegerField()
+    type=models.ForeignKey(type_emtiyaz, on_delete=models.SET_NULL,null=True)
+    create_at=jmodels.jDateTimeField()
+    Time_period=models.ForeignKey(Time_period, on_delete=models.CASCADE)
+    own_user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='own_user')
+    user_register=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,related_name='user_register')
+
+# Create your models here.
