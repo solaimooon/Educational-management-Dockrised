53af979402a780abfcf7fd788ba222e70a116a36
solaimooon <118716931+solaimooon@users.noreply.github.com>
2025-02-27 00:03:01 +0330

    just create the creation of compattions

--- compattions/admin.py
+++ compattions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class compattions_admin(admin.ModelAdmin):
+    list_display=['name','monasebat']
+
+admin.site.register(compattions,compattions_admin)
+
--- compattions/models.py
+++ compattions/models.py
@@ -1,3 +1,16 @@
+from email.mime import audio, image
+from email.policy import default
+from os import name
+from tkinter.messagebox import QUESTION
 from django.db import models
 
+class compattions(models.Model):
+    name=models.CharField(max_length=50)
+    question=models.CharField(max_length=500)
+    monasebat=models.CharField(max_length=50)
+    link=models.URLField()
+    image=models.ImageField( upload_to='compatitons/',null=True,default="NULL")
+    audio_or_video=models.FileField(upload_to='compatitons/', max_length=100 ,null=True,default="NULL")
+    def __str__(self):
+        return self.name
 # Create your models here.
