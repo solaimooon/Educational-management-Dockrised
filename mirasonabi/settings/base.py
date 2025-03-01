cd789251caa1ba4470dfab43e96a56c5d3d6f3a8
solaimooon <118716931+solaimooon@users.noreply.github.com>
2025-03-01 16:15:45 +0330

    create the new index

--- mirasonabi/settings/base.py
+++ mirasonabi/settings/base.py
@@ -144,6 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static_file')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- website/views.py
+++ website/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def index(request):
 
-    return render(request,'website/index.html')
+    return render(request,'new_template/index.html')
 # Create your views here.
