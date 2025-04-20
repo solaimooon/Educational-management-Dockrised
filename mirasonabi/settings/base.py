f528c62767b98af6c3f56de54c02045bf2b59f7b
solaimooon <118716931+solaimooon@users.noreply.github.com>
2025-04-20 11:18:03 +0330

    debhg the time picker that show wrong date

--- mirasonabi/settings/base.py
+++ mirasonabi/settings/base.py
@@ -152,6 +152,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # jalali date picker setting
+# default settings (optional)
 JALALI_DATE_DEFAULTS = {
    # if change it to true then all dates of the list_display will convert to the Jalali.
    'LIST_DISPLAY_AUTO_CONVERT': False,
@@ -161,23 +162,19 @@ JALALI_DATE_DEFAULTS = {
     },
     'Static': {
         'js': [
-            # loading datepicker
             'admin/js/django_jalali.min.js',
-            # OR
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
-            # 'admin/js/main.js',
         ],
         'css': {
             'all': [
-                'admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.min.css',
+              'admin/css/django_jalali.min.css',
             ]
         }
     },
 }
 
+
+    
+    
 # crispy setting
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
